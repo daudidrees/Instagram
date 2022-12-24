1db14b196354416c002c3ea482f61639e09605a8
daudidrees <m.daudidrees@gmail.com>
2022-12-24 22:44:39 +0500

    added desktop home page

---
+++ dist/assets/script/app.js
@@ -0,0 +1,28 @@
+const navSearch = document.querySelector('.navSearch');
+const faq = document.querySelectorAll('.faqLi');
+const faqMessage =  document.querySelectorAll('.faqs-message');
+
+if(navSearch != null){
+  navSearch.addEventListener('click', ()=> {
+    document.querySelector('.navSearchWrapper > input').style.opacity = 1;
+  })
+}
+
+for(let i = 0; i < faq.length; i++){
+  faq[i].addEventListener('click', () => {
+    faqMessage.forEach((el, index) => {
+      if(i == index){
+        el.classList.toggle('hidden')
+      }
+    })
+  })
+}
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
