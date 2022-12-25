7b9c06f80d02e818c48d2cdd5cbff86d18057d21
daudidrees <m.daudidrees@gmail.com>
2022-12-25 16:03:57 +0500

    added swiper responsive

--- dist/assets/script/app.js
+++ dist/assets/script/app.js
@@ -19,10 +19,22 @@ for(let i = 0; i < faq.length; i++){
 }
 
 var swiper = new Swiper(".mySwiper", {
-  slidesPerView: 3,
+  slidesPerView: 1,
   spaceBetween: 30,
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
   },
+
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+    },
+
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    }
+  }
 });
