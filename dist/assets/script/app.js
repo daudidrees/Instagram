7b81f4c800a58825db6c35c8f8031093f7cd2b32
daudidrees <m.daudidrees@gmail.com>
2022-12-28 23:30:20 +0500

    added some feature

--- dist/assets/script/app.js
+++ dist/assets/script/app.js
@@ -1,6 +1,8 @@
 const navSearch = document.querySelector('.navSearch');
 const faq = document.querySelectorAll('.faqLi');
 const faqMessage =  document.querySelectorAll('.faqs-message');
+const headerBtn = document.getElementById('headerBtn')
+const headerMbl = document.querySelector('.headerBtn');
 
 if(navSearch != null){
   navSearch.addEventListener('click', ()=> {
@@ -18,6 +20,18 @@ for(let i = 0; i < faq.length; i++){
   })
 }
 
+if(headerBtn != null) {
+  headerBtn.addEventListener('click', function(){
+    document.getElementById('dropdown').classList.toggle('hidden')
+  })
+}
+
+if(headerMbl != null) {
+  headerMbl.addEventListener('click', function(){
+    document.querySelector('.mbl-dropdown').classList.toggle('hidden')
+  })
+}
+
 var swiper = new Swiper(".mySwiper", {
   slidesPerView: 1,
   spaceBetween: 30,
@@ -25,6 +39,10 @@ var swiper = new Swiper(".mySwiper", {
     el: ".swiper-pagination",
     clickable: true,
   },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
 
   breakpoints: {
     768: {
@@ -34,7 +52,7 @@ var swiper = new Swiper(".mySwiper", {
 
     1024: {
       slidesPerView: 3,
-      spaceBetween: 30,
+      spaceBetween: 60,
     }
   }
 });
